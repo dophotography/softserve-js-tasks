de9d8f3bbff71071b8bcefb1a21dc5e9ad82ab02
Dmytro Ohanov <dmitryohanovphotography@gmail.com>
2023-06-22 17:18:54 +0100

    task 1 completed

---
+++ scripts/task-1.js
@@ -0,0 +1,34 @@
+//2
+console.log('Ohanov')
+
+//6
+const myName = 'Dima'
+const mySurname = 'Ohanov'
+const myGroup = '001'
+const yearOfBirth = 2004
+const maritalStatus = confirm('Are you married?')
+
+console.log(typeof yearOfBirth, yearOfBirth)
+console.log(typeof maritalStatus, maritalStatus)
+console.log(typeof myName, myName)
+
+const nullValue = null
+const undefinedValue
+
+console.log(typeof nullValuem)
+console.log(typeof undefinedValuem)
+
+//7
+const username = prompt('Enter you username:')
+const email = prompt('Enter yout email:')
+const password = prompt('Enter your password:')
+alert(`Dear ${username}, your email is ${email}, your password is ${password}`)
+
+//8
+const secInHour = 60 * 60;
+const secInDay = secInHour * 24;
+const secInMonth = secInDay * 30;
+
+console.log(`Seconds in hour: ${secInHour}`)
+console.log(`Seconds in day: ${secInDay}`)
+console.log(`Seconds in month: ${secInMonth}`)
