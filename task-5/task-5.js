559097d8e1fccf97987786936bed2015e87fb6da
Dmytro Ohanov <dmitryohanovphotography@gmail.com>
2023-07-12 20:24:16 +0100

    5 task completed

--- task-5/task-5.js
+++ task-5/task-5.js
@@ -77,81 +77,117 @@ const outputContainer = document.getElementById('output')
 // outputContainer.innerHTML += `${student.showFullName('Mary')}` + `${student.showCourse()}`
 
 //4
-class Marker {
-    constructor(color, inkLevel) {
-        this.color = color
-        this.inkLevel = inkLevel
-    }
+// class Marker {
+//     constructor(color, inkLevel) {
+//         this.color = color
+//         this.inkLevel = inkLevel
+//     }
 
-    async printText(text) {
-        let outputHtml = ''
-        let i = 0
+//     async printText(text) {
+//         let outputHtml = ''
+//         let i = 0
 
-        do {
-            if (this.inkLevel <= 0) {
-                outputHtml += `<span class="out-of-ink"><br><br>Marker is out of ink.</span>`
-                break
-            }
+//         do {
+//             if (this.inkLevel <= 0) {
+//                 outputHtml += `<span class="out-of-ink"><br><br>Marker is out of ink.</span>`
+//                 break
+//             }
 
-            const char = text[i]
+//             const char = text[i]
 
-            if (char !== ' ') {
-                if (this.inkLevel >= 0.5) {
-                    outputHtml += `<span style="color:${this.color}">${char}</span>`
-                    this.inkLevel -= 0.5
-                }
-            } else {
-                outputHtml += ' '
-            }
+//             if (char !== ' ') {
+//                 if (this.inkLevel >= 0.5) {
+//                     outputHtml += `<span style="color:${this.color}">${char}</span>`
+//                     this.inkLevel -= 0.5
+//                 }
+//             } else {
+//                 outputHtml += ' '
+//             }
 
-            i++
-        } while (i < text.length)
+//             i++
+//         } while (i < text.length)
 
-        outputContainer.innerHTML += outputHtml
-    }
-}
+//         outputContainer.innerHTML += outputHtml
+//     }
+// }
 
-class RefillableMarker extends Marker {
-    constructor(color, inkLevel) {
-        super(color, inkLevel)
-    }
+// class RefillableMarker extends Marker {
+//     constructor(color, inkLevel) {
+//         super(color, inkLevel)
+//     }
 
-    async refillMarker(ink) {
-        this.inkLevel += ink
-        alert(`Marker is refilled for ${ink} and current ink level is ${this.inkLevel}`)
-    }
-}
+//     async refillMarker(ink) {
+//         this.inkLevel += ink
+//         alert(`Marker is refilled for ${ink} and current ink level is ${this.inkLevel}`)
+//     }
+// }
 
-let text
-let color
-let inkLevel
+// let text
+// let color
+// let inkLevel
 
-document.getElementById('submitText').addEventListener('click', async (event) => {
-    event.preventDefault()
+// document.getElementById('submitText').addEventListener('click', async (event) => {
+//     event.preventDefault()
 
-    const form = document.getElementById('textForm')
-    const textInput = form.elements.textInput
-    const textColor = form.elements.textColor
-    const markerCapacity = form.elements.markerCapacity
+//     const form = document.getElementById('textForm')
+//     const textInput = form.elements.textInput
+//     const textColor = form.elements.textColor
+//     const markerCapacity = form.elements.markerCapacity
 
-    text = textInput.value
-    color = textColor.value
-    inkLevel = parseInt(markerCapacity.value)
+//     text = textInput.value
+//     color = textColor.value
+//     inkLevel = parseInt(markerCapacity.value)
 
-    const marker = new Marker(color, inkLevel)
-    await marker.printText(text)
-})
+//     const marker = new Marker(color, inkLevel)
+//     await marker.printText(text)
+// })
 
-document.getElementById('submitRefill').addEventListener('click', async (event) => {
-    event.preventDefault()
+// document.getElementById('submitRefill').addEventListener('click', async (event) => {
+//     event.preventDefault()
 
-    const form = document.getElementById('refillForm')
-    const refillAmount = form.elements.refillAmount
+//     const form = document.getElementById('refillForm')
+//     const refillAmount = form.elements.refillAmount
 
-    const inkAmount = refillAmount.value
+//     const inkAmount = refillAmount.value
 
-    const refillableMarker = new RefillableMarker(color, inkLevel)
-    await refillableMarker.refillMarker(inkAmount)
-})
+//     const refillableMarker = new RefillableMarker(color, inkLevel)
+//     await refillableMarker.refillMarker(inkAmount)
+// })
 
 //5
+class Worker {
+
+    #experience
+
+    constructor(fullName, dayRate, workingDays) {
+        this.fullName = fullName
+        this.dayRate = dayRate
+        this.workingDays = workingDays
+    }
+
+    showSalary() {
+        return this.dayRate * this.workingDays
+    }
+
+    showSalaryWithExperience() {
+        return this.showSalary() * this.#experience
+    }
+
+    getExperience() {
+        return this.#experience
+    }
+
+    setExperience(workerExperience) {
+        this.#experience = workerExperience
+        console.log(`Worker experience: ${this.#experience}`)
+    }
+
+}
+
+document.getElementById('show-salary').addEventListener('click', async (event) => {
+    event.preventDefault()
+
+    const worker = new Worker('Mikola', 20, 21)
+    outputContainer.innerHTML += worker.showSalaryWithExperience(1.2)
+
+})
