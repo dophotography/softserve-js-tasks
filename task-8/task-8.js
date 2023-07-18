d5f2f4afe337456392e244819c19460469f83067
Dmytro Ohanov <dmitryohanovphotography@gmail.com>
2023-07-18 21:20:08 +0100

    module 8 done

--- task-8/task-8.js
+++ task-8/task-8.js
@@ -6,28 +6,62 @@ function upperCase(str) {
         upperCaseOutput.textContent = `String's not starts with uppercase character`
 }
 
-upperCase('Matter')
+upperCaseBtn.onclick = () => {
+    upperCase(upperCaseInput.value)
+}
 
 //2
 function checkEmail(email) {
     const regexpEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
     regexpEmail.test(email) ?
-        checkEmailOutput.textContent = 'true' :
-        checkEmailOutput.textContent = 'false'
+    emailOutput.textContent = 'true' :
+        emailOutput.textContent = 'false'
 }
 
-checkEmail('Qmail2@')
+emailBtn.onclick = () => {
+    checkEmail(emailInput.value)
+}
 
 //3
-const substringInputText = document.querySelectorAll('substringInput')
-const substringChangeBtn = document.getElementById('substringChangeBtn')
-
-function swapSubstring(text, substring1, substring2) {
-    const regex = new RegExp(`(${substring1})(.*)(${substring2})`)
-    substringOutput.textContent = text.replace(`$3$2$1`)
+function swapSubstring(text) {
+    const regexpSubstring = /(\w+)\s(\w+)/
+    substringOutput.textContent = text.replace(regexpSubstring, '$2, $1')
 }
 
 substringChangeBtn.addEventListener('click', () => {
-    swapSubstring(substringTextInput.value, substringFirstWord.value, substringSecondWord.value)
+    swapSubstring(substringTextInput.value)
 })
 
+//4
+function cardValidation(cardNumber) {
+    const regexpCardNumber = /[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}/
+    regexpCardNumber.test(cardNumber) ?
+        checkCardNumberOutput.textContent = `Card number is valid` : checkCardNumberOutput.textContent = `Card number is not valid`
+}
+
+cardNumberValidation.onclick = () => {
+    cardValidation(cardNumberInput.value)
+}
+
+//5
+function emailValidation(email) {
+    const regexpEmailValidation = /[A-Za-z0-9]+([_\-.][A-Za-z0-9]+)*@[A-Za-z0-9]+([_\-.][A-Za-z0-9]+)*\.[A-Za-z]{2,}/
+    regexpEmailValidation.test(email) ? 
+        emailValidationOutput.textContent = `Email is correct` : emailValidationOutput.textContent = `Email is not correct`
+}
+
+emailValidationBtn.onclick = () => {
+    emailValidation(emailValidationInput.value)
+}
+
+//6
+function loginValidation(login) {
+    const regexpLoginValidation = /[A-Za-z][A-Za-z0-9]{1,9}/
+    const numbers = login.match(/[0-9.]+/g)
+    regexpLoginValidation.test(login) ? 
+        loginValidationOutput.textContent = `Login is correct. Numbers in login: ${numbers}` : loginValidationOutput.textContent = `Email is not correct. Numbers in login: ${numbers}`
+}
+
+loginValidationBtn.onclick = () => {
+    loginValidation(loginValidationInput.value)
+}
